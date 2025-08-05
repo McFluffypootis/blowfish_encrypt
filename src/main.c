1a0cff29fe8252ddaa4f25a7f4eddc5758d0fd77
McFluffypootis <ziganshintimur1.5@gmail.com>
2025-08-05 16:49:44 +0200

    file input and options added

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
+#include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
 
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 uint32_t P[18];
 uint32_t S[4][256];
@@ -43,14 +47,13 @@ void blowfish_decrypt(uint32_t *L, uint32_t *R) {
   *L = *L ^ P[0];
 }
 
-int main(int argc, char *argv[]) {
-  int key_position = 0;
-  int key_length = 4;
+FILE *fp;
 
-  char key[] = "ABCD";
+void blowfish_init(char key[]) {
+  int key_position = 0;
+  int key_length = 576;
 
   int i = 0;
-
   for (i = 0; i < 18; i++) {
     int k = 0;
     for (int j = 0; j < 18; j++) {
@@ -75,21 +78,65 @@ int main(int argc, char *argv[]) {
       S[i][j + 1] = R;
     }
   }
+}
 
-  L = 0x01234567;
-  R = 0x01234567;
+int main(int argc, char *argv[]) {
 
+  enum { MODE_ENCRYPT, MODE_DECRYPT } mode = MODE_ENCRYPT;
 
-  printf("L: %x R: %x \n", L, R);
+  char key[576];
 
-  blowfish_encrypt(&L, &R);
+  int opt;
+  while ((opt = getopt(argc, argv, "edk:")) != -1) {
+    switch (opt) {
+    case 'd':
+      mode = MODE_DECRYPT;
+      break;
+    case 'e':
+      mode = MODE_ENCRYPT;
+      break;
+    case 'k':
+      strcpy(key, optarg);
+      break;
+    case 'p':
+      break;
+    default:
+      fprintf(stderr, "Usage %s [-de] [file...] [-k] [key...]\n", argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  }
 
-  printf("L: %x R: %x \n", L, R);
+  if (optind < argc) {
+    printf("optind %s \n", argv[optind]);
 
-  blowfish_decrypt(&L, &R);
+    if ((fp = fopen(argv[optind], "r")) == NULL) {
+      fprintf(stderr, "%s: failed to open %s (%d %s)\n", argv[0], argv[optind],
+              errno, strerror(errno));
+    }
+  }
 
-  printf("L: %x R: %x \n", L, R);
+  blowfish_init(key);
 
+  char chunk[10];
+
+  uint32_t L;
+  uint32_t R;
+
+  if (mode == MODE_ENCRYPT) {
+    while (fgets(chunk, 10, fp)) {
+      L = *(uint32_t *)chunk;
+      R = *(uint32_t *)(chunk + 4);
+      printf("L: %x R: %x \n", L, R);
+      blowfish_encrypt(&L, &R);
+    }
+  } else if (mode == MODE_DECRYPT) {
+    while (fgets(chunk, 10, fp)) {
+      L = *(uint32_t *)chunk;
+      R = *(uint32_t *)(chunk + 4);
+      printf("L: %x R: %x \n", L, R);
+      blowfish_decrypt(&L, &R);
+    }
+  }
 
   return 0;
 }
